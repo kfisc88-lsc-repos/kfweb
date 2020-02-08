e4a9c5c68c90493df83d2cbfa34e3219d6cc84a2
arkais <krfisc@outlook.com>
2020-02-08 13:36:45 -0600

    Finished boostrapping my site

---
+++ fwd_bootstrap.php
@@ -0,0 +1,21 @@
+<?php include 'templates/header.html' ?>
+
+<!-- Heading -->
+<div class="container">
+    <h1 class="text-center">Assignments</h1>
+    <hr class="header-hr mb-5">
+</div>
+<!-- /Heading -->
+
+<!-- Assingments -->
+<div class="container">
+    <div class="list-group">
+      <a href="assignments/KFischer_Week1.docx" class="list-group-item list-group-item-action" download>Week 1 - Company Website Outline</a>
+      <a href="assignments/KFischer_wk2_creative_brief.docx" class="list-group-item list-group-item-action" download>Week 2 - Creative Brief</a>
+      <a href="cis-1408\WEB\bs\b4hp01\index.html" class="list-group-item list-group-item-action">Week 3 - Bootstrap Creative site from book</a>
+      <a href="#" class="list-group-item list-group-item-action">Week 4 - Bootstrapped Company Website</a>
+    </div>
+</div>
+<!-- /Assignments -->
+
+<?php include 'templates/footer.html' ?>
--- fwd_index.php
+++ fwd_index.php
@@ -2,7 +2,7 @@
 
 <!-- Heading -->
 <div class="container">
-    <h1 class="mt-5 pt-5 text-center">Welcome to Fischer Web Design</h1>
+    <h1 class="text-center">Welcome to Fischer Web Design</h1>
     <hr class="header-hr">
     <h5 class="pb-5 text-center">The home for all my projects and assignments.</h5>
 </div>
@@ -14,30 +14,30 @@
         <div class="col mb-4">
             <div class="card mx-auto h-100 w-100" style="width: 18rem;">
                 <img src="img/gmhero.png" class="card-img-top" alt="Screenshot of GM Hero Homepage">
-                <div class="card-body">
+                <div class="card-body shadow">
                     <h5 class="card-title">GM Hero (Original)</h5>
                     <p class="card-text">A site for managing tabletop RPGs</p>
-                    <a href="mywebsite/gmh_index.html" class="btn bgc-secondary fc-quinary">Check it out!</a>
+                    <a href="mywebsite/gmh_index.html" class="btn font-weight-bold bgc-secondary fc-primary">Check it out!</a>
                 </div>
             </div>
         </div>
         <div class="col mb-4">
             <div class="card mx-auto h-100 w-100" style="width: 18rem;">
                 <img src="img/bootstrapcreative.png" class="card-img-top" alt="Screenshot of Bootstrap Creative Homepage">
-                <div class="card-body">
+                <div class="card-body shadow">
                     <h5 class="card-title">Bootstrap Creative</h5>
                     <p class="card-text">Assignment website</p>
-                    <a href="cis-1408\WEB\bs\b4hp01\index.html" class="btn bgc-secondary fc-quinary mt-4">Check it out!</a>
+                    <a href="cis-1408\WEB\bs\b4hp01\index.html" class="btn font-weight-bold bgc-secondary fc-primary mt-4">Check it out!</a>
                 </div>
             </div>
         </div>
         <div class="col mb-4">
             <div class="card mx-auto h-100 w-100" style="width: 18rem;">
                 <img src="img/gmhero.png" class="card-img-top" alt="Screenshot of GM Hero Homepage">
-                <div class="card-body">
+                <div class="card-body shadow">
                     <h5 class="card-title">GM Hero (Bootstrap)</h5>
                     <p class="card-text">Bootstrapped version of GM Hero</p>
-                    <a href="#" class="btn bgc-secondary fc-quinary">Check it out!</a>
+                    <a href="#" class="btn font-weight-bold bgc-secondary fc-primary">Check it out!</a>
                 </div>
             </div>
         </div>
