9a360408527e539a3d7f27d1ff5987e35dc38dea
arkais <krfisc@outlook.com>
2020-02-08 12:59:51 -0600

    Made significant changes using boostrap. Added php to create new pages.

---
+++ php/fwd_bootstrap.php
@@ -0,0 +1,17 @@
+<?php include 'templates/header.html' ?>
+
+<!-- Assignment-Grid -->
+<div class="container">
+    <h5>Assignments</h5>
+    <hr>
+    <div class="row">
+        <div class="col">
+
+        </div>
+
+    </div>
+
+</div>
+<!-- /Assignment-Grid -->
+
+<?php include 'templates/footer.html' ?>
---
+++ php/fwd_index.php
@@ -0,0 +1,48 @@
+<?php include 'templates/header.html' ?>
+
+<!-- Heading -->
+<div class="container">
+    <h1 class="mt-5 pt-5 text-center">Welcome to Fischer Web Design</h1>
+    <hr class="header-hr">
+    <h5 class="pb-5 text-center">The home for all my projects and assignments.</h5>
+</div>
+<!-- /Heading -->
+
+<!-- Cards -->
+<div class="container">
+    <div class="row row-cols-1 row-cols-lg-3">
+        <div class="col mb-4">
+            <div class="card mx-auto h-100 w-100" style="width: 18rem;">
+                <img src="img/gmhero.png" class="card-img-top" alt="Screenshot of GM Hero Homepage">
+                <div class="card-body">
+                    <h5 class="card-title">GM Hero (Original)</h5>
+                    <p class="card-text">A site for managing tabletop RPGs</p>
+                    <a href="mywebsite/gmh_index.html" class="btn bgc-secondary fc-quinary">Check it out!</a>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card mx-auto h-100 w-100" style="width: 18rem;">
+                <img src="img/bootstrapcreative.png" class="card-img-top" alt="Screenshot of Bootstrap Creative Homepage">
+                <div class="card-body">
+                    <h5 class="card-title">Bootstrap Creative</h5>
+                    <p class="card-text">Assignment website</p>
+                    <a href="cis-1408\WEB\bs\b4hp01\index.html" class="btn bgc-secondary fc-quinary mt-4">Check it out!</a>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card mx-auto h-100 w-100" style="width: 18rem;">
+                <img src="img/gmhero.png" class="card-img-top" alt="Screenshot of GM Hero Homepage">
+                <div class="card-body">
+                    <h5 class="card-title">GM Hero (Bootstrap)</h5>
+                    <p class="card-text">Bootstrapped version of GM Hero</p>
+                    <a href="#" class="btn bgc-secondary fc-quinary">Check it out!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Cards -->
+
+<?php include 'templates/footer.html' ?>
